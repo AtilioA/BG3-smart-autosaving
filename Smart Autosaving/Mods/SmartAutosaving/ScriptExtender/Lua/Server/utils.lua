88cb5173038102d824a578ba59c001295df0287d
Atílio <atiliodadalto@protonmail.com>
2024-01-20 14:38:04 -0300

    refactor: improve `DebugPrint()` formatting

--- Smart Autosaving/Mods/SmartAutosaving/ScriptExtender/Lua/Server/utils.lua
+++ Smart Autosaving/Mods/SmartAutosaving/ScriptExtender/Lua/Server/utils.lua
@@ -2,7 +2,11 @@ Utils = {}
 
 function Utils.DebugPrint(level, ...)
   if JsonConfig and JsonConfig.DEBUG and JsonConfig.DEBUG.level >= level then
+    if (JsonConfig.DEBUG.level == 0) then
       print(...)
+    else
+      print("[Smart Autosaving][DEBUG LEVEL " .. level .. "]: " .. ...)
+    end
   end
 end
 
